bc947c99766c23b32cbaaf29b0eb53f7ceac2c35
Git-ijhwang <hwangij@gmail.com>
2024-12-14 21:54:08 -0400

    modify udp send

--- src/gtpv2_send.rs
+++ src/gtpv2_send.rs
@@ -1,26 +1,31 @@
-use std::io;
-use std::sync::Arc ;
 use std::net::UdpSocket;
 
 use crate::gtp_msg::*;
 use crate::timer::*;
 use crate::peers::*;
-use crate::gtpv2_ie::*;
-
 
 
 pub fn send_udp_data(data: &[u8], ip: &str, port: u16)
--> io::Result<()> {
+-> Result<usize, String> {
     // Create a new UDP socket
-    let socket = UdpSocket::bind("0.0.0.0:0")?; // Binds to an available port on the local machine
+    // Binds to an available port on the local machine
+    let socket;
+    let ret = UdpSocket::bind("0.0.0.0:0");
+    match ret {
+        Ok(v) => socket = v,
+        _ => return Err ("Socket Open Error occoured".to_string()),
+    }
+
 
     // Format the target address
     let addr = format!("{}:{}", ip, port);
 
     // Send the data
-    socket.send_to(data, addr)?;
-
-    Ok(())
+    let ret = socket.send_to(data, addr);
+    match ret {
+        Ok(v) => Ok(v),
+        _ => Err ("Send Error occoured".to_string()),
+    }
 }
 
 
@@ -46,14 +51,23 @@ pub async fn make_gtpv2(msg_type: u8, body: &[u8;1024],
 
     let pkt = EncapPkt::new(peer.ip, msg_type, buffer[..length + len as usize].to_vec());
 
-    send_udp_data(&buffer[..length+len as usize], &peer.ip.to_string(), peer.port);
+    let ret = send_udp_data(&buffer[..length+len as usize], &peer.ip.to_string(), peer.port);
+    match ret {
+        Ok(v) => {
+            if v <= 0 {
+                return  Err ("Fail to send message to peer".to_string());
+            }
 
-    let mut queue_locked = SHARED_QUEUE.lock().await;
-    queue_locked.push(pkt);
+            let mut queue_locked = SHARED_QUEUE.lock().await;
+            queue_locked.push(pkt);
 
-    peer.update_last_echo_snd_time();
-    peer.increase_count();
+            peer.update_last_echo_snd_time();
+            peer.increase_count();
 
-    Ok(())
+            Ok(())
+        },
+        _ => return  Err ("Fail to send message to peer".to_string()),
+    }
+
 }
 
